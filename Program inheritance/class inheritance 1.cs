6d1a7f9bf352f6ce5a15088ef03aa895f45fbbf9
Farikhajhrm <92256707+Farikhajhrm@users.noreply.github.com>
2021-10-10 20:07:25 +0700

    Add files via upload

---
+++ Program inheritance/class inheritance 1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter11hal305
+{
+    public class KomisiKaryawan : Object
+    {
+        public String NamaDepan { get; }
+        public String NamaBelakang { get; }
+        public String SocialSecurityNumber { get; }
+        private decimal penjualanKotor; //penjualan mingguan kotor
+        private decimal tarifKomisi; //presentase komisi
+
+        //konstruktor lima parameter
+        public KomisiKaryawan(String namaDepan, string namaBelakang, string socialSecurityNumber, decimal penjualanKotor, decimal tarifKomisi)
+        {
+        // panggilan implisit ke konstruktor objek terjadi di sini
+        NamaDepan = namaDepan;
+        NamaBelakang = namaBelakang;
+        SocialSecurityNumber = socialSecurityNumber;
+        PenjualanKotor = penjualanKotor; //memvalidasi penjualan kotor
+        TarifKomisi = tarifKomisi; //memvalidasi tingkat komisi
+        }
+        // properti yang mendapatkan dan menetapkan komisi penjualan kotor karyawan
+         public decimal PenjualanKotor
+         {
+            get
+            {
+              return penjualanKotor;
+            }
+            set
+            {
+              if (value < 0) //memvalidasi
+              {
+                throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(penjualanKotor)} harus >= 0");
+              }
+              penjualanKotor = value;
+            }
+         }
+         // properti yang mendapatkan dan menetapkan tingkat komisi komisi karyawan
+         public decimal TarifKomisi
+         {
+             get
+             {
+               return tarifKomisi;
+             }
+             set
+             {
+               if (value <= 0 || value >= 1) //memvalidasi
+               {
+                 throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(TarifKomisi)} harus > 0 and < 1");
+               }
+               tarifKomisi = value;
+             }
+         }
+         // hitung komisi gaji pegawai
+         public decimal Pendapatan() => tarifKomisi * penjualanKotor;
+
+         // mengembalikan representasi string dari objek CommissionEmployee
+         public override string ToString() =>
+         $"komisikaryawan: {NamaDepan} {NamaBelakang}\n" + $"social security number: {SocialSecurityNumber}\n" + $"penjualan kotor: { penjualanKotor:C}\n" + $"tarif komisi: {tarifKomisi:F2}";
+    }
+    class TesKomisiKaryawan
+    {
+        static void Main()
+        {
+            //memberi contoh objek KomisiKaryawan
+            var karyawan = new KomisiKaryawan("Sue", "Jones", "222-22-2222", 10000.00M, .06M);
+            //tampilkan data Komisi Karyawan
+            Console.WriteLine("Informasi karyawan diperoleh dengan properti dan metode: \n");
+            Console.WriteLine($"Nama depan adalah {karyawan.NamaDepan}");
+            Console.WriteLine($"Nama belakang adalah {karyawan.NamaBelakang}");
+            Console.WriteLine($"Nomor Social security adalah {karyawan.SocialSecurityNumber}");
+            Console.WriteLine($"Penjualan kotor adalah {karyawan.PenjualanKotor:C}");
+            Console.WriteLine($"Tarif komisi adalah {karyawan.TarifKomisi:F2}");
+            Console.WriteLine($"Pendapatan adalah {karyawan.Pendapatan():C}");
+
+            karyawan.PenjualanKotor = 5000.00M; //menetapkan penjualan kotor
+            karyawan.TarifKomisi = .1M; //menetapkan tarif komisi
+
+            Console.WriteLine("\nInformasi karyawan yang diperbarui diperoleh oleh ToString:\n");
+            Console.WriteLine(karyawan);
+            Console.WriteLine($"penghasilan: {karyawan.Pendapatan():C}");
+        }
+    }
+}
---
+++ Program polimorfisme/polimorfisme 1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter12_1
+{
+    public class KomisiKaryawan
+    {
+        public string NamaAwal { get; }
+        public string NamaAkhir { get; }
+        public string NoKTP { get; }
+        private decimal penjualanKotor; // penjualan kotor mingguan
+        private decimal nilaiKomisi; // persentase nilai komisi
+        // lima parameter kontruktor
+        public KomisiKaryawan(string namaAwal, string namaAkhir, string noKtp, decimal penjualanKotor, decimal nilaiKomisi)
+        {
+            // memanggil implisit ke objek konstruktor di sini
+            NamaAwal = namaAwal;
+            NamaAkhir = namaAkhir;
+            NoKTP = noKtp;
+            PenjualanKotor = penjualanKotor; // validasi penjualan kotor
+            NilaiKomisi = nilaiKomisi; // validasi nilai komisi
+        }
+        // atribut yang mendapatkan dan menetapkan komisi penjualan kotor karyawan
+        public decimal PenjualanKotor
+        {
+            get
+            {
+                return penjualanKotor;
+            }
+            set
+            {
+                if (value < 0) // validasi
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(PenjualanKotor)} harus >= 0");
+                }
+                penjualanKotor = value;
+            }
+        }
+        // atribut yang mendapatkan dan menetapkan tingkat atau nilai komisi karyawan
+        public decimal NilaiKomisi
+        {
+            get
+            {
+                return nilaiKomisi;
+            }
+            set
+            {
+                if (value <= 0 || value >= 1) // validasi
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(NilaiKomisi)} harus > 0 dan < 1");
+                }
+                nilaiKomisi = value;
+            }
+        }
+
+        // menghitung pembayaran komisi karyawan
+        public virtual decimal Penghasilan() => NilaiKomisi * PenjualanKotor;
+        // mengembalikan representasi string dari objek KomisiKaryawan
+        public override string ToString() =>
+        $"komisi karyawan: {NamaAwal} {NamaAkhir}\n" + $"no ktp: {NoKTP}\n" + $"penjualan kotor: {PenjualanKotor:C}\n" + $"nilai komisi: {NilaiKomisi:F2}";
+    }
+        public class KomisiTambahanKaryawan : KomisiKaryawan
+        {
+            private decimal gajiPokok; // gaji pokok per minggunya
+
+            // enam parameter konstruktor kelas turunan 
+            // dengan memanggil ke kelas dasar konstruktor KomisiKaryawan
+            public KomisiTambahanKaryawan(string namaAwal, string namaAkhir, string noKtp, decimal grossSales, decimal nilaiKomisi, decimal gajiPokok)
+    : base(namaAwal, namaAkhir, noKtp, grossSales, nilaiKomisi)
+            {
+                GajiPokok = gajiPokok; // validasi gaji pokok
+            }
+            // atribut get dan set
+            // gaji tambahan karyawan
+            public decimal GajiPokok
+            {
+                get
+                {
+                    return gajiPokok;
+                }
+                set
+                {
+                    if (value < 0) // validasi
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(GajiPokok)} harus >= 0");
+                    }
+                    gajiPokok = value;
+                }
+            }
+            // calculate earnings
+            public override decimal Penghasilan() => GajiPokok + base.Penghasilan();
+
+            // mengembalikan representasi string dari KomisiTambahanKaryawan
+            public override string ToString() => $"gaji pokok {base.ToString()}\ngaji pokok: {GajiPokok:C}";
+        }
+    class PolymorphismTest
+    {
+        static void Main()
+        {
+            // menetapkan referensi kelas dasar ke variabel kelas dasar
+            var komisiKaryawan = new KomisiKaryawan("Sue", "Jones", "222-22-2222", 10000.00M, .06M);
+
+            // menetapkan referensi kelas turunan ke variabel kelas turunan
+            var komisiTambahanKaryawan = new KomisiTambahanKaryawan("Bob", "Lewis", "333-33-3333", 5000.00M, .04M, 300.00M);
+
+            // mengaktifkan ToString dan Penghasilan pada objek kelas dasar
+            // menggunakan variabel kelas dasar
+            Console.WriteLine("Memanggil metode ToString dan Penghasilan Komisi Karyawan " + "dengan referensi kelas dasar ke objek kelas dasar\n");
+            Console.WriteLine(komisiKaryawan.ToString());
+            Console.WriteLine($"penghasilan: {komisiKaryawan.Penghasilan()}\n");
+
+            // mengaktifkan ToString dan Penghasilan pada objek kelas turunan
+            // menggunakan variabel kelas turunan
+            Console.WriteLine("Memanggil metode komisi tambahan karyawan dan" + " metode penghasilan dengan referensi kelas turunan ke" + " objek kelas turunan\n");
+            Console.WriteLine(komisiTambahanKaryawan.ToString());
+            Console.WriteLine($"earnings: {komisiTambahanKaryawan.Penghasilan()}\n");
+
+            // mengaktifkan ToString dan Penghasilan pada objek kelas turunan
+            // menggunakan variabel kelas dasar
+            KomisiKaryawan komisiKaryawan1 = komisiTambahanKaryawan;
+            Console.WriteLine("Memanggil komisi tambahan karyawan dan penghasilan " + "metode dengan referensi kelas dasar ke objek kelas turunan");
+            Console.WriteLine(komisiKaryawan1.ToString());
+            Console.WriteLine($"earnings: {komisiTambahanKaryawan.Penghasilan()}\n");
+        }
+    }
+
+    }
